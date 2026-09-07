7d9e764e32e398332bb99960fe598da3dd189f44
yashbhardwaj001 <yashbhardwaj509@gmail.com>
2026-09-08 00:26:33 +0530

    update the changes

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({
+// CORS(Cross-Origin Resource Sharing) :- Backend ko permission deta hai ki kaunse frontend/domain se API request accept karni hai.
+
+app.use(cors({ 
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-
+ 
 // some configurations needed :-
 
 app.use(express.json({limit: "16kb"})) // json data lene k liye
@@ -16,6 +18,11 @@ app.use(express.urlencoded({extended: true,limit:"16kb"})) //url se data lene k 
 app.use(express.static("public")) // images wagera public me rakne k liye 
 app.use(cookieParser())
 
+// routes import 
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+app.use("/api/v1/users",userRouter)  // ==> http://localhost:8000/api/v1/users/register
 
 
 export {app}
